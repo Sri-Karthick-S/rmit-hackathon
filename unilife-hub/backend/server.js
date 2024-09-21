98d10e8fd6893c233ebd4a78398e6c688e122b75
luffina <s3977421@student.rmit.edu.au>
2024-09-21 11:56:39 +1000

    Your commit message

--- unilife-hub/backend/server.js
+++ unilife-hub/backend/server.js
@@ -3,7 +3,27 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db'); // Import the db connection
 const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
+// server.js (add to existing file)
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on('message', (data) => {
+    io.to(data.roomId).emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 dotenv.config(); // Load env variables
 connectDB(); // Connect to MongoDB
 
